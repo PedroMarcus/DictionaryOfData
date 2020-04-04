ee7856d9204bfd3eda0e1584b763164692449cca
Pedro Silva <pedro.silva@ivory.com.br>
2020-04-04 14:21:04 -0300

    Criação de projeto
    Projeto de Processos e de teste criados.

---
+++ src/app/DictionaryOfData/Dictionary.UnitTest/Connection/ConnectOnDatabase.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary.UnitTest.Connection
+{
+    public class ConnectOnDatabase
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void VerifyInputsRequired() 
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TryConnectOnDatabaseSqlServer() 
+        {
+            Assert.Pass(); 
+        }
+
+        [Test]
+        public void TryConnectOnDatabaseOracle() 
+        {
+            Assert.Pass(); 
+        }
+
+        [Test]
+        public void TryConnectOnDatabaseMySQL() 
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ src/app/DictionaryOfData/Dictionary.UnitTest/Creation/StructureOnData.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary.UnitTest
+{
+    public class StructureOnData
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CreationsOfTables()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void VerifyExistenceOfTables()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public string BuildEstructureForTables()
+        {
+            return string.Empty;
+        }
+    }
+}
